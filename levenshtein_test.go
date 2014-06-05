5f1d859d7e3d4f20739b4cc3cd77fc1bc6cfea67
masatana@157-7-141-207 <plaza.tumbling@gmail.com>
2014-06-05 16:46:14 +0900

    Implement jaro distance

--- levenshtein_test.go
+++ levenshtein_test.go
@@ -2,13 +2,13 @@ package textdistance
 
 import "testing"
 
-type LevenshteinTest struct {
+type LevenshteinDistanceTest struct {
 	s1       string
 	s2       string
 	expected int
 }
 
-var LevenshteinTests = []LevenshteinTest{
+var LevenshteinDistanceTests = []LevenshteinDistanceTest{
 	{
 		"kitten",
 		"sitting",
@@ -31,8 +31,8 @@ var LevenshteinTests = []LevenshteinTest{
 	},
 }
 
-func TestContains(t *testing.T) {
-	for _, lt := range LevenshteinTests {
+func TestLevenshteinDistance(t *testing.T) {
+	for _, lt := range LevenshteinDistanceTests {
 		actual := LevenshteinDitance(lt.s1, lt.s2)
 		if lt.expected != actual {
 			t.Errorf("Levenshtein distance of %s and %s: want %d got %d", lt.s1, lt.s2, lt.expected, actual)
