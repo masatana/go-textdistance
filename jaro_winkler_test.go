4874f8542a94f9078df8899f1c716892dadd780d
masatana <plaza.tumbling@gmail.com>
2014-06-05 16:55:32 -0700

    Added jaro_winkler_test

---
+++ jaro_winkler_test.go
@@ -0,0 +1,37 @@
+package textdistance
+
+type jaroWinkerDistanceTest struct {
+	s1       string
+	s2       string
+	expected float64
+}
+
+func (jdt *jaroWinkerDistanceTest) equals(dis float64) bool {
+	if jdt.expected-1e-3 < dis && jdt.expected < jdt.expected+1e-3 {
+		return true
+	}
+	return false
+}
+
+var JaroWinklerDistanceTests = []jaroWinkerDistanceTest{
+	{
+		"MARTHA",
+		"MARHTA",
+		0.961,
+	},
+	{
+		"DWAYNE",
+		"DUANE",
+		0.84,
+	},
+	{
+		"DIXON",
+		"DICKSONX",
+		0.813,
+	},
+	{
+		"あいうえおか",
+		"あいうおえか",
+		0.961,
+	},
+}
