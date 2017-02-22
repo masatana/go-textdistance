7a669211081295dc8d65d6ad21c02ce96a4c6bcb
Masahiro Tanaka <plaza.tumbling@gmail.com>
2017-02-22 23:45:17 +0900

    Fix Maximum Integer

--- util.go
+++ util.go
@@ -1,8 +1,10 @@
 package textdistance
 
+import "math"
+
 // Min returns the minimum number of passed int slices.
 func Min(is ...int) int {
-	min := int(1e10)
+	min := int(math.MaxInt64)
 	for _, v := range is {
 		if min > v {
 			min = v
